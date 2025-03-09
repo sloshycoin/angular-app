5a76c4e41900b8cfb3591866d06c4f55568c5d3d
sloshycoin <brendanburgee@gmail.com>
2025-03-08 18:37:32 -0700

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, LandingComponent],
-  template: '<app-header/><app-landing/><app-footer/>',
+  imports: [HeaderComponent, NavbarComponent, LandingComponent, FooterComponent],
+  template: '<app-header/><app-navbar/><app-landing/><app-footer/>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
