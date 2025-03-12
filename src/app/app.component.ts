aa7efd8f88f2c507bb30ea130d7cc4f5f4d81e5e
sloshycoin <brendanburgee@gmail.com>
2025-03-12 17:05:54 -0600

    swap to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, FormsModule],
-  template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-  `,
+  imports: [ RouterOutlet, FormsModule, HeaderComponent, FooterComponent ],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
